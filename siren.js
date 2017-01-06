325fe54bad026745e7d21d8b761c0bfacfe8e660
Scott Luptowski <scottluptowski@gmail.com>
2017-01-06 15:58:56 -0500

    Adding board to script

--- siren.js
+++ siren.js
@@ -1,5 +1,8 @@
+const Tessel = require("tessel-io")
+const five = require("johnny-five")
 const Twit = require("twit")
 const colors = require("colors/safe")
+
 const dotenv = require("dotenv")
 dotenv.config()
 
@@ -11,16 +14,34 @@ const T = new Twit({
 })
 
 const TWITTER_USER_ID = "816025856892141569"
-const stream = T.stream("statuses/filter", { follow: TWITTER_USER_ID })
 
-stream.on("connect", () => {
-  console.log(colors.underline("Attempting to connect"))
+const board = new five.Board({
+  io: new Tessel()
 })
 
-stream.on("connected", (response) => {
-  console.log(colors.green("Connected!"))
-})
+board.on("ready", () => {
+  console.log("ready")
+  const relay = new five.Relay({pin: "b7", type: "NC"})
+  console.log("relay created")
 
-stream.on("tweet", (tweet) => {
-  console.log(tweet)
+  relay.on()
+  console.log("relay turned on")
+
+  setTimeout(() => {
+    relay.off()
+    console.log("relay turned off")
+  }, 3000)
+
+  const stream = T.stream("statuses/filter", { follow: TWITTER_USER_ID })
+  stream.on("connect", () => {
+    console.log(colors.underline("Attempting to connect"))
+  })
+
+  stream.on("connected", (response) => {
+    console.log(colors.green("Connected!"))
+  })
+
+  stream.on("tweet", (tweet) => {
+    console.log(tweet)
+  })
 })
