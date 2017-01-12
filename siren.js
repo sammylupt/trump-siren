270d83eac898d0ca16155c6b5a461b610c52a04a
Scott Luptowski <scottluptowski@gmail.com>
2017-01-12 14:35:21 -0500

    Working prototype

--- siren.js
+++ siren.js
@@ -1,4 +1,3 @@
-const Tessel = require("tessel-io")
 const five = require("johnny-five")
 const Twit = require("twit")
 const colors = require("colors/safe")
@@ -13,39 +12,39 @@ const T = new Twit({
   access_token_secret:  process.env.ACCESS_TOKEN_SECRET
 })
 
-const TWITTER_USER_ID = "816025856892141569"
+const TWITTER_ID = process.env.TEST_MODE ? "816025856892141569" : "25073877"
 
 const log = (text) => {
   console.log(`${new Date()}: ${text}`)
 }
 
-const board = new five.Board({
-  io: new Tessel()
+const board = new five.Board()
+
+board.on("ready", function(){
+  const relay = new five.Relay(7)
+  log("Relay initialized. Testing light!")
+  toggleLight(relay, 1000)
+
+  const stream = T.stream("statuses/filter", { follow: TWITTER_ID })
+  stream.on("connect", () => {
+    log(colors.underline("Attempting to connect"))
+  })
+
+  stream.on("connected", (response) => {
+    log(colors.green("Connected! Listening for tweets"))
+  })
+
+  stream.on("tweet", (tweet) => {
+    toggleLight(relay, 5000)
+  })
 })
 
-board.on("ready", () => {
-  log("ready")
-  const relay = new five.Relay({pin: "b7", type: "NC"})
-  log("relay created")
-
+const toggleLight = (relay, ms) => {
   relay.on()
   log(colors.red("Light switched on"))
 
   setTimeout(() => {
     relay.off()
     log(colors.red("Light switched off"))
-  }, 3000)
-
-  const stream = T.stream("statuses/filter", { follow: TWITTER_USER_ID })
-  stream.on("connect", () => {
-    log(colors.underline("Attempting to connect"))
-  })
-
-  stream.on("connected", (response) => {
-    log(colors.green("Connected!"))
-  })
-
-  stream.on("tweet", (tweet) => {
-    console.log(tweet)
-  })
-})
+  }, ms)
+}
